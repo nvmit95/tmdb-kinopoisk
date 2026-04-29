cbad2086aa28c43e452e7df7033f1d59dfcb9915
Nikita Mitsin <nvmit95@gmail.com>
2026-04-29 14:56:15 +0300

    Add main padding

--- src/pages/main/ui/MainPage.styles.ts
+++ src/pages/main/ui/MainPage.styles.ts
@@ -3,6 +3,7 @@ import { pxToRem } from "@/shared/theme"
 export const MAIN_PAGE_SX = {
   main: {
     bgcolor: "background.default",
+    paddingBottom: pxToRem(64),
   },
   stack: {
     gap: pxToRem(56),
